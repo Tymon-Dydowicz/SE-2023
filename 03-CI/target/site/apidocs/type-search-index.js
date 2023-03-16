3d86839a28934f823b89dd7264b4028d85fa2b6c
somethingwentwrongx <tymon.dydowicz@onet.pl>
2023-03-17 00:04:05 +0100

    Removing target folder

--- 03-CI/target/site/apidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"put.io.selenium.atm","l":"AtmCardInfo"},{"p":"put.io.selenium.atm","l":"AtmServlet"}];updateSearchResults();
--- 03-CI/target/site/testapidocs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"put.io.selenium.atm","l":"AtmCardInfoTest"}];updateSearchResults();
